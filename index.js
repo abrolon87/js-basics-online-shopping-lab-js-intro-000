90e900cbbaa0b0c4c5c193d60dcd53da3522c641
Amanda R <abrolon87@gmail.com>
2019-08-19 16:20:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function placeOrder(cardNumber) {
   if (cardNumber) {
     for (let i = cart.length; i >= 0; i--) {
      cart.pop();
-    
+
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
